9c8c15d0ffcc3106d94443020a15b734b0f0f284
nguye <nguye@NGUYEN_HUNG01>
2021-10-30 12:58:53 +0700

    Add project files.

---
+++ WebMvcBooks/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvcBooks.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my  default  action...";
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
